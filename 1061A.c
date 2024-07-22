854bc1caa1c3278090976f694266af9f890e0130
khoat1k31 <89307671+khoat1k31@users.noreply.github.com>
2024-07-22 08:14:22 +0700

    Update 1061A.c

--- 1061A.c
+++ 1061A.c
@@ -3,7 +3,17 @@ int main()
 {
     int n,s;
     scanf("%d %d", &n, &s);
-    int total= (s/n) +1;
+    if (s%n==0)
+    {
+    int total= (s/n);
     printf("%d", total);
+    }
+    else if(s%n!=0)
+    {
+        int total=(s/n)+1;
+        printf("%d", total);
+    }
+
+
     return 0;
 }
