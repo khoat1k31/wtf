e25a85ff554fd4e67916561b16b2d2a555a097c9
khoat1k31 <89307671+khoat1k31@users.noreply.github.com>
2024-08-03 08:28:10 +0700

    Add files via upload

---
+++ minmax4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    int max1=-1e9-1, max2=-1e9-1;
+    for (int i=0; i<n; i++)
+    {
+        if(a[i]>max1)
+        {
+            max2=max1;
+            max1=a[i];
+        }
+        if(a[i]>max2 && a[i]!=max1)
+        {
+            max2=a[i];
+        }
+    }
+    for (int i=0; i<n; i++)
+    {
+        if(a[i]<max1 && a[i]<max2)
+            printf("%d ", a[i]);
+    }
+    return 0;
+}
