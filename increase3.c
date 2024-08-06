a7f9c14e8a290dd3d2f66e2d673280dfc89e1c73
khoat1k31 <89307671+khoat1k31@users.noreply.github.com>
2024-08-06 22:45:04 +0700

    Add files via upload

---
+++ increase3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void sx(int a[], int n)
+{
+    int temp;
+    for (int i=0; i<n-1; i++ )
+    {
+        for (int j=i+1; j<n; j++){
+            if (a[i]>a[j]){
+                temp=a[j];
+                a[j]=a[i];
+                a[i]=temp;
+            }
+        }
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i=0; i<n; i++)
+        scanf("%d", &a[i]);
+    sx(a, n);
+    for (int i=0; i<n; i++)
+        printf("%d ", a[i]);
+    return 0;
+}
