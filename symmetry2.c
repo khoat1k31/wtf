babe5d68be8403197b8d43b6f01434fa3aafaa8f
khoat1k31 <89307671+khoat1k31@users.noreply.github.com>
2024-08-07 08:54:31 +0700

    Add files via upload

---
+++ symmetry2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int check(int a[], int n)
+{
+    for (int i=0; i<n/2; i++) {
+            if (a[i]!=a[n-i-1]) return 0;
+    }
+    return 1;
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i=0; i<n; i++)
+        scanf("%d", &a[i]);
+    if (check(a,n)) printf("YES");
+    else printf("NO");
+    return 0;
+}
